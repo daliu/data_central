5b3269385238ac92a8a91744a04391ca0ea976bb
David Liu <daveliu@berkeley.edu>
2015-10-25 04:08:52 -0700

    initial files

---
+++ capital_one_api.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+customerId = 'e7bff3ff405d7b4e32cc80be8a8f1d37'
+apiKey = ''
+
+url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId,apiKey)
+payload = {
+  "type": "Savings",
+  "nickname": "test",
+  "rewards": 10000,
+  "balance": 10000,	
+}
+# Create a Savings Account
+response = requests.post( 
+	url, 
+	data=json.dumps(payload),
+	headers={'content-type':'application/json'},
+	)
+
+if response.status_code == 201:
+	print('account created')
